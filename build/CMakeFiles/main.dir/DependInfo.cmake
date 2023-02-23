2a739e0e70b3fa9c133d7537c5b733a382106a54
AndreiPascu180 <78821603+AndreiPascu180@users.noreply.github.com>
2023-02-23 23:04:10 +0200

    Add files via upload

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/eu/Desktop/Develop/PSOP-Debugger/ext/linenoise/linenoise.c" "/home/eu/Desktop/Develop/PSOP-Debugger/build/CMakeFiles/main.dir/ext/linenoise/linenoise.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../ext/libelfin"
+  "../ext/linenoise"
+  "../include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/eu/Desktop/Develop/PSOP-Debugger/breakpoint.cpp" "/home/eu/Desktop/Develop/PSOP-Debugger/build/CMakeFiles/main.dir/breakpoint.cpp.o"
+  "/home/eu/Desktop/Develop/PSOP-Debugger/debugger.cpp" "/home/eu/Desktop/Develop/PSOP-Debugger/build/CMakeFiles/main.dir/debugger.cpp.o"
+  "/home/eu/Desktop/Develop/PSOP-Debugger/main.cpp" "/home/eu/Desktop/Develop/PSOP-Debugger/build/CMakeFiles/main.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../ext/libelfin"
+  "../ext/linenoise"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
